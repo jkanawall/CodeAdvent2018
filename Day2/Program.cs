21f0434d4dccfff45090de6ae95ca8c50b743832
joeyak <joeyak97@gmail.com>
2018-12-21 17:32:34 -0500

    Day2 Part1

---
+++ Day2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var checkTwo = 0;
+            var checkThree = 0;
+            foreach (var line in File.ReadAllLines("input.txt"))
+            {
+                var counts = line.GroupBy(x => x).Select(x => x.Count());
+                if (counts.Contains(2))
+                {
+                    checkTwo++;
+                }
+                if (counts.Contains(3))
+                {
+                    checkThree++;
+                }
+            }
+
+            Console.WriteLine($"{checkTwo} * {checkThree} = {checkTwo * checkThree}");
+        }
+    }
+}
