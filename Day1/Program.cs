13ee5c2d830cafa87b7337e9ad070582fba8fd32
joeyak <joeyak97@gmail.com>
2018-12-20 19:26:51 -0500

    Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        private static HashSet<int> reachedFrequencies = new HashSet<int> { 0 };
+        static void Main(string[] args)
+        {
+            var result = ParseFrequencies(0);
+            Console.WriteLine($"Frequency: {result.freq}");
+            Console.WriteLine($"First Repeat: {result.rep}");
+        }
+
+        static (int freq, int? rep) ParseFrequencies(int frequency)
+        {
+            int? repeat = null;
+            foreach (var num in File.ReadAllLines("input.txt"))
+            {
+                frequency += Convert.ToInt32(num);
+                if (repeat is null && !reachedFrequencies.Add(frequency))
+                {
+                    repeat = frequency;
+                }
+            }
+
+            if (repeat is null)
+            {
+                (_, repeat) = ParseFrequencies(frequency);
+            }
+            return (frequency, repeat);
+        }
+    }
+}
